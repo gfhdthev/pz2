57eb08e94edbe593a7376ac8a1468425bee776b0
gfhdthev <akkglow@tutamail.com>
2025-03-14 09:19:12 +0300

    ....

--- task_2.py
+++ task_2.py
@@ -1,14 +1,18 @@
 #Gfhdthev
 #Практическое занятие, задание 2
 
+import matplotlib as mtl
 import matplotlib.pyplot as plt
 import matplotlib.patches
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
 class Circle:
-    def __init__(self, x, y, r):
+    def __init__(self, x, y, z, r):
         self.x = x
         self.y = y
         self.r = r
+        self.z = z
 
     def graf(self):
 
@@ -25,5 +29,30 @@ class Circle:
     def __del__(self):
         print('Окружность удалена')
 
-first = Circle(0, 0, 5)
+
+
+class Ball(Circle):
+    def __init__(self, x, y, z, r):
+        super().__init__(x, y, r)
+        self.z = z
+
+    def graf(self):
+        self.fig = plt.figure(figsize=(10,10))
+        self.ax = self.fig.add_subplot(111, projection = '3d')
+
+        self.u = np.linspace(0, 2 * np.pi, 100)
+        self.v = np.linspace(0, np.pi, 100)
+
+        self.x = self.r * np.outer(np.cos(self.u), np.sin(self.v)) + self.x
+        self.y = self.r * np.outer(np.sin(self.u), np.sin(self.v)) + self.y
+        self.z = self.r * np.outer(np.ones(np.size(self.u)), np.cos(self.v)) + self.z
+
+        self.ax.plot_surface(self.x, self.y, self.z, color='b')
+
+        plt.show()
+
+    def __del__(self):
+        print(f'Шар удален')
+
+first = Ball(0, 0, 0, 5)
 first.graf()
